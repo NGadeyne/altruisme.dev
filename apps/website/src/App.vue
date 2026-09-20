1537247c016ee1708c4b7f4865180ad7487b02c2
NGadeyne <nicolas.g@thewalkingnerds.com>
2026-09-20 12:36:46 +0200

    fix(website): mount cookie consent after hydration

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import CookieConsent from '@/components/cookies/CookieConsent.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
+
+const isMounted = ref(false)
+
+onMounted(() => {
+  isMounted.value = true
+})
 </script>
 
 <template>
   <DefaultLayout>
     <RouterView />
-    <CookieConsent />
+    <CookieConsent v-if="isMounted" />
   </DefaultLayout>
 </template>
