c0381e673bb95ef898d61cd91a74917201b41de6
NGadeyne <nicolas.g@thewalkingnerds.com>
2026-09-26 11:38:01 +0200

    feat(builder): polish SEO landing page

--- apps/builder/src/App.vue
+++ apps/builder/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BuilderLanding from '@/components/BuilderLanding.vue'
 import CookieConsent from '@/components/CookieConsent.vue'
-const mailto = 'mailto:contact@altruisme.dev?subject=Projet%20Altruisme%20Builder'
+const mailto = 'mailto:contact@altruisme.dev?subject=Faire%20le%20point%20sur%20mon%20SEO'
 </script>
-<template><BuilderLanding :action-href="mailto" action-label="Analyser mon site" /><CookieConsent /></template>
+<template><BuilderLanding :action-href="mailto" action-label="Faire le point sur mon SEO" /><CookieConsent /></template>
